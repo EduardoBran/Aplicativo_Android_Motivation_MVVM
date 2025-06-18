cae5aa601bf3d989984d4dd7ed94e0a9ef34d3fd
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-18 16:47:15 -0300

    criando repositorios e tornando UserActivity como activity principal

--- app/src/main/java/com/luizeduardobrandao/motivationmvvm/ui/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/motivationmvvm/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.luizeduardobrandao.motivationmvvm
+package com.luizeduardobrandao.motivationmvvm.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.luizeduardobrandao.motivationmvvm.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
